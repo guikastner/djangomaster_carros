33170edc78c86ef8846dab8fbb086186c3cd5b02
guikastner <guilherme@kastner.com.br>
2025-01-07 17:36:51 -0300

    models - cars

--- cars/models.py
+++ cars/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    brand = models.CharField(max_length=200)
+    model = models.CharField(max_length=200)
+    factory_year = models.IntegerField(blank=True,null=True)
+    model_year = models.IntegerField(blank=True,null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    value = models.FloatField(blank=True,null=True)
