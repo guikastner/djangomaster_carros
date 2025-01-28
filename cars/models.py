80a5c273dc8cef44a5a589acb2941dc18aaec590
guikastner <guilherme@kastner.com.br>
2025-01-28 13:18:50 -0300

    change on models

--- cars/models.py
+++ cars/models.py
@@ -9,8 +9,8 @@ class Brand(models.Model):
 
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
-    brand = models.CharField(max_length=200)
-    model = models.ForeignKey('Brand', on_delete=models.PROTECT,related_name='car_cbrand')
+    model = models.CharField(max_length=200)
+    brand = models.ForeignKey('Brand', on_delete=models.PROTECT,related_name='car_brand')
     factory_year = models.IntegerField(blank=True,null=True)
     model_year = models.IntegerField(blank=True,null=True)
     value = models.FloatField(blank=True,null=True)
