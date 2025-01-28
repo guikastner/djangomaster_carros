2418d7460b757bb83b9e2eda8a6d9e333f43c34a
guikastner <guilherme@kastner.com.br>
2025-01-28 13:14:28 -0300

    update admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Car
+from cars.models import Car, Brand	# Import the Car and Brand models
 
 class CarAdmin(admin.ModelAdmin): 
     list_display = ('brand', 'model', 'factory_year', 'model_year', 'value')
     list_filter = ('brand', 'model', 'factory_year', 'model_year', 'value')
     search_fields = ['model', 'brand']
 
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
 
-admin.site.register(Car, CarAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Car, CarAdmin)	# Register the Car and Brand models with the admin site
