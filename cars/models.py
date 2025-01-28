4d6efe010dc15310c9ed2c7a6a872e6a64a0d0a3
guikastner <guilherme@kastner.com.br>
2025-01-28 13:08:07 -0300

    new class

--- cars/models.py
+++ cars/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self): # Generate a string representation of the model
+        return self.name
 
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
     brand = models.CharField(max_length=200)
-    model = models.CharField(max_length=200)
+    model = models.ForeignKey('Brand', on_delete=models.PROTECT,related_name='car_cbrand')
     factory_year = models.IntegerField(blank=True,null=True)
     model_year = models.IntegerField(blank=True,null=True)
     value = models.FloatField(blank=True,null=True)
@@ -13,3 +18,4 @@ class Car(models.Model):
 
     def __str__(self): # Generate a string representation of the model
         return self.model
+     
