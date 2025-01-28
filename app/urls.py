d10bfad90e37d1350591f0a91c0a737064e4a974
guikastner <guilherme@kastner.com.br>
2025-01-28 15:23:00 -0300

    fix url

--- app/urls.py
+++ app/urls.py
@@ -4,13 +4,12 @@ import os
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
-def carview():
-    return 'Meus carros'
+def carview(request):
+    return HttpResponse('Meus carros')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', carview),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
